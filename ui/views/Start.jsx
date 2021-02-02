52348823711f7b33fb093270aa4d57566b2ab6f8
Thomas Schranz <thomas@magic.do>
2021-02-02 19:31:14 +0100

    add create new room button

--- ui/views/Start.jsx
+++ ui/views/Start.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 
 export default function Start() {
+  var randomId = Math.random().toString(36).substr(2, 6);
+  var roomHref = "/" + randomId;
   return (
     <div className="container">
       <div className="child">
-        <h1>Jam</h1>
+        <h1>Welcome to Jam</h1>
         <p>
           <img alt="Jam Logo" src="/img/jam-logo.jpg" />
         </p>
+        <form action={roomHref} method="get">
+          <button href={roomHref}>🌱 Create a new room</button>
+        </form>
       </div>
     </div>
   );
