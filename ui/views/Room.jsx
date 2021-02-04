69ccd567c00cc0089d25ecfec537d38bd753cccd
Thomas Schranz <thomas@magic.do>
2021-02-04 14:05:49 +0100

    room: stage ui as ol instead of table

--- ui/views/Room.jsx
+++ ui/views/Room.jsx
@@ -19,44 +19,35 @@ export default function Room() {
         <h1>Reddit vs Hedge Funds</h1>
 
         <h3 style={{marginTop: '80px'}}>Stage</h3>
-        <table className="stage">
-          <tbody>
-            <tr>
-              {myStream && (
-                <td className={speaking.has('me') ? 'speaking' : undefined}>
-                  <img src="img/avatars/tosh.jpg" />
-                  <span
-                    className={
-                      speaking.has('me')
-                        ? 'visible animate-ping flex h-3 w-10 relative top-0 right-0 -mt-6'
-                        : 'invisible animate-ping flex h-3 w-10 relative top-0 right-0 -mt-6'
-                    }
-                  >
-                    <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-600 opacity-75"></span>
-                    <span className="relative inline-flex rounded-full h-3 w-10 bg-green-800"></span>
-                  </span>
-                </td>
-              )}
-              {streams.map(({stream, peerId}) =>
-                !stream ? undefined : (
-                  <td
-                    key={peerId}
-                    className={speaking.has(peerId) ? 'speaking' : undefined}
-                    title={peerId}
-                    alt={peerId}
-                  >
-                    <img
-                      className={
-                        speaking.has(peerId) ? 'animate-ping' : undefined
-                      }
-                      src="img/avatars/sonic.jpg"
-                    />
-                  </td>
-                )
-              )}
-            </tr>
-          </tbody>
-        </table>
+        <ol className="flex space-x-4 pt-6">
+        {myStream && (
+          <li className="flex-shrink w-28 h-28 text-center">
+            <img className="human-radius" src="img/avatars/sonic.jpg" />
+            <span
+              className={
+                speaking.has('me')
+                  ? 'visible'
+                  : 'invisible'
+              }
+            >speaking</span>
+          </li>
+        )}
+        {streams.map(({stream, peerId}) =>
+          !stream ? undefined : (
+            <li
+              key={peerId}
+              className="flex-shrink w-28 h-28 text-center"
+              title={peerId}
+              alt={peerId}
+            >
+              <img className="human-radius" src="img/avatars/sonic.jpg" />
+              <span className={speaking.has(peerId) ? 'visible' : 'invisible'}>
+                speaking
+              </span>
+            </li>
+          )
+        )}
+        </ol>
 
         <h3 style={{marginTop: '80px'}}>Audience</h3>
         <table className="audience">
