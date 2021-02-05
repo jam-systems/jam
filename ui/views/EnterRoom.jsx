e186ebbe267bc45923a39147d7b79d22130401d0
Thomas Schranz <thomas@magic.do>
2021-02-05 01:09:54 +0100

    enter-room: hide unused buttons, remove color transitions

--- ui/views/EnterRoom.jsx
+++ ui/views/EnterRoom.jsx
@@ -7,25 +7,21 @@ export default function EnterRoom({roomId, name}) {
       <div className="child">
         <h1 className="font-book text-black text-xl3">{name || ''}</h1>
         {/*
-            re-using the "audience" class here for now to display speakers
-            might make sense to switch to more generic names
-            like avatar-big, avatar-medium and so on
-
-            this can be a snapshot of current speakers or a snapshot of
-            anticipated speakers for scheduled (future) rooms
+            a snapshot of current or nticipated speakers
+            (for scheduled (future) rooms)
         */}
         <ol className="flex space-x-4 pt-6">
           <li className="flex-shrink w-28 h-28 ring-yellow-500">
-            <img className="human-radius" src="img/avatars/sonic.jpg" />
+            <img className="human-radius border border-gray-300" src="img/avatars/sonic.jpg" />
           </li>
           <li className="flex-shrink w-28 h-28">
-            <img className="human-radius" src="img/avatars/gregor.jpg" />
+            <img className="human-radius border border-gray-300" src="img/avatars/gregor.jpg" />
           </li>
           <li className="flex-shrink w-28 h-28">
-            <img className="human-radius" src="img/avatars/christoph.jpg" />
+            <img className="human-radius border border-gray-300" src="img/avatars/christoph.jpg" />
           </li>
           <li className="flex-shrink w-28 h-28">
-            <img className="human-radius" src="img/avatars/tosh.jpg" />
+            <img className="human-radius border border-gray-300" src="img/avatars/tosh.jpg" />
           </li>
         </ol>
         <p className="text-gray-500">
@@ -36,7 +32,7 @@ export default function EnterRoom({roomId, name}) {
             optional (for future events:)
             when is this event?
         */}
-        <p className="pt-4 pb-4">
+        <p className="hidden pt-4 pb-4">
           🗓 February 3rd 2021 at ⌚️ 14:06 (Vienna Time)
         </p>
         {/*
@@ -54,18 +50,18 @@ export default function EnterRoom({roomId, name}) {
         */}
         <button
           onClick={() => enterRoom(roomId)}
-          className="h-12 px-6 m-2 text-lg text-black transition-colors duration-150 bg-gray-300 rounded-lg focus:shadow-outline hover:bg-gray-400"
+          className="mt-10 h-12 px-6 text-lg text-black bg-gray-200 rounded-lg focus:shadow-outline hover:bg-gray-300"
         >
           🚪 Join this room
         </button>
         {/*
             if it is a future/scheduled room this button could be replaced with
         */}
-        <button className="h-12 px-6 m-2 text-lg text-black transition-colors duration-150 bg-gray-300 rounded-lg focus:shadow-outline hover:bg-gray-400">
+        <button className="hidden h-12 px-6 text-lg text-black bg-gray-200 rounded-lg focus:shadow-outline hover:bg-gray-300">
           ⏰ Alert me 5 min before
         </button>
 
-        <button className="h-12 px-6 m-2 text-lg text-black transition-colors duration-150 bg-gray-300 rounded-lg focus:shadow-outline hover:bg-gray-400">
+        <button className="hidden h-12 px-6 text-lg text-black bg-gray-200 rounded-lg focus:shadow-outline hover:bg-gray-300">
           🗓 Add this to my calendar
         </button>
       </div>
