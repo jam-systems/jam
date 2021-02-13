7de62899ea6720c9136f13387e84aca476ce17b3
Thomas Schranz <thomas@magic.do>
2021-02-13 19:30:11 +0100

    move navigation to fixed position in bottom of room screen

--- ui/views/Room.jsx
+++ ui/views/Room.jsx
@@ -76,7 +76,83 @@ export default function Room({room, roomId}) {
     return <EnterRoom roomId={roomId} name={name} description={description} />;
 
   return (
-    <div className="container">
+    <div className="container min-h-screen">
+      {/* Navigation
+      
+      */}
+      <div className="z-10 navigation absolute bottom-0 bg-white p-4 pb-8">
+        <div className="flex">
+          <button
+            onClick={() => state.set('micMuted', !micMuted)}
+            className="select-none h-12 mt-4 px-6 text-lg text-black bg-yellow-200 rounded-lg focus:shadow-outline active:bg-yellow-300 flex-grow"
+          >
+            {micOn
+              ? micMuted
+                ? "🙊 You're silent"
+                : "🐵 You're on"
+              : "🙊 You're off"}
+          </button>
+        </div>
+
+        <br />
+
+        <div className="flex">
+          <button
+            onClick={() => state.set('soundMuted', !soundMuted)}
+            className="mr-2 select-none h-12 px-6 text-lg text-black bg-gray-200 rounded-lg focus:shadow-outline active:bg-gray-300 flex-grow"
+          >
+            {soundMuted ? '🔇' : '🔊'} {soundMuted ? 'Off' : 'On'}
+          </button>
+
+          {/* Share */}
+          {showShareInfo && (
+            <span
+              style={{
+                position: 'absolute',
+                top: '-20px',
+                left: '2px',
+                fontSize: '13px',
+              }}
+            >
+              Link copied to clipboard!
+            </span>
+          )}
+          <button
+            onClick={() => {
+              if (navigator.share) {
+                navigator.share({
+                  title: name || 'A Jam room',
+                  text: 'Hi, join me in this room on Jam.',
+                  url: location.href,
+                });
+              } else {
+                copyToClipboard(location.href);
+                setShowShareInfo(true);
+                setTimeout(() => setShowShareInfo(false), 2000);
+              }
+            }}
+            className="ml-2 mr-2 select-none h-12 px-6 text-lg text-black bg-gray-200 rounded-lg focus:shadow-outline active:bg-gray-300"
+          >
+            ✉️&nbsp;Share
+          </button>
+
+          {/* Leave */}
+          <button
+            className="ml-2 select-none h-12 px-6 text-lg text-black bg-gray-200 rounded-lg focus:shadow-outline active:bg-gray-300"
+            onClick={() => leaveRoom(roomId)}
+          >
+            🚪 Leave quietly
+          </button>
+        </div>
+
+        <div className="flex relative">
+          <button className="select-none hidden h-12 px-6 text-lg text-black bg-gray-200 rounded-lg focus:shadow-outline active:bg-gray-300 flex-grow">
+            ✋🏽&nbsp;Raise&nbsp;hand
+          </button>
+        </div>
+
+      </div>
+
       {editIdentity && (
         <EditIdentity
           info={myInfo}
@@ -94,227 +170,155 @@ export default function Room({room, roomId}) {
           onCancel={() => setEditRole(null)}
         />
       )}
-      <div className="child md:p-10">
+      <div className="child flex flex-col h-5/6 md:p-10">
         <h1 className="pl-2 pt-6 md:pt-0">{name}</h1>
         <div className="pl-2 text-gray-500">{description}</div>
 
-        {/* Stage */}
-        <div className="h-44 h-full">
-          <ol className="flex flex-wrap space-x-2 pt-6">
-            {iSpeak && (
+        {/* Main Area */}
+        <div className="">
+          {/* Stage */}
+          <div className="">
+            <ol className="flex flex-wrap space-x-2 pt-6">
+              {iSpeak && (
+                <li
+                  className="relative items-center space-y-1 mt-4"
+                  style={{cursor: 'pointer'}}
+                  onClick={() => setEditIdentity(!editIdentity)}
+                >
+                  <div
+                    className={
+                      speaking.has('me')
+                        ? 'human-radius p-1 bg-gray-300'
+                        : 'human-radius p-1 bg-white'
+                    }
+                  >
+                    <div className="human-radius p-1 bg-white">
+                      <img
+                        className="human-radius border border-gray-300 bg-yellow-50 w-20 h-20 md:w-28 md:h-28"
+                        alt="me"
+                        src={gravatarUrl(myInfo)}
+                      />
+                    </div>
+                  </div>
+                  <div className={micMuted ? '' : 'hidden'}>
+                    <div className="absolute w-10 h-10 right-0 top-12 md:top-20 rounded-full bg-white border-2 text-2xl border-gray-400 flex items-center justify-center">
+                      🙊
+                    </div>
+                  </div>
+                  <div className="font-medium w-20 md:w-28 m-2 break-words">
+                    <div className="flex">
+                      <div style={{lineHeight: "30px", marginTop: "4px"}} className={moderators.includes(swarm.myPeerId) ? "flex-none block bg-yellow-400 text-white font-light text-4xl w-4 h-4 text-center rounded-full" : "hidden"}>*</div>
+                      <div className="flex-none pl-1 overflow-ellipsis w-20 md:w-28">{myInfo.displayName}</div>
+                    </div>
+                  </div>
+                </li>
+              )}
+              {stagePeers.map(peerId => {
+                let {micMuted, inRoom} = peerState[peerId] || {};
+                const peerInfo = identities[peerId] || {id: peerId};
+                // TODO: hadStream is NOT the appropriate condition for showing avatar
+                // need inRoom status from peers
+                return (
+                  inRoom && (
+                    <li
+                      key={peerId}
+                      className="relative items-center space-y-1 mt-4"
+                      title={peerId}
+                      style={iModerate ? {cursor: 'pointer'} : undefined}
+                      onClick={iModerate ? () => setEditRole(peerId) : undefined}
+                    >
+                      <div
+                        className={
+                          speaking.has(peerId)
+                            ? 'human-radius p-1 bg-gray-300'
+                            : 'human-radius p-1 bg-white'
+                        }
+                      >
+                        <div className="human-radius p-1 bg-white">
+                          <img
+                            className="human-radius border border-gray-300 bg-yellow-50 w-20 h-20 md:w-28 md:h-28"
+                            alt={peerId}
+                            src={gravatarUrl(peerInfo)}
+                          />
+                        </div>
+                      </div>
+                      {/* div for showing mute/unmute status */}
+                      <div className={micMuted ? '' : 'hidden'}>
+                        <div className="absolute w-10 h-10 right-0 top-12 md:top-20 rounded-full bg-white border-2 text-2xl border-gray-400 flex items-center justify-center">
+                          🙊
+                        </div>
+                      </div>
+                      <div className="font-medium text-center w-20 md:w-28 m-2 break-words">
+                        <div className="flex">
+                          <div style={{lineHeight: "30px", marginTop: "4px"}} className={moderators.includes(peerId) ? "flex-none block bg-yellow-400 text-white font-light text-4xl w-4 h-4 text-center rounded-full" : "hidden"}>*</div>
+                          <div className="flex-none pl-1 truncate w-20 md:w-28">{peerInfo.displayName}</div>
+                        </div>
+                      </div>
+                    </li>
+                  )
+                );
+              })}
+            </ol>
+          </div>
+
+          <br />
+
+          <h3 className="text-gray-300">Audience</h3>
+          <ol className="flex space-x-4 pt-6">
+            {!iSpeak && (
               <li
-                className="relative items-center space-y-1 mt-4"
+                className="flex-shrink w-16 h-16 md:w-24 md:h-24 text-xs"
                 style={{cursor: 'pointer'}}
                 onClick={() => setEditIdentity(!editIdentity)}
               >
-                <div
-                  className={
-                    speaking.has('me')
-                      ? 'human-radius p-1 bg-gray-300'
-                      : 'human-radius p-1 bg-white'
-                  }
-                >
-                  <div className="human-radius p-1 bg-white">
-                    <img
-                      className="human-radius border border-gray-300 bg-yellow-50 w-20 h-20 md:w-28 md:h-28"
-                      alt="me"
-                      src={gravatarUrl(myInfo)}
-                    />
-                  </div>
-                </div>
-                <div className={micMuted ? '' : 'hidden'}>
-                  <div className="absolute w-10 h-10 right-0 top-12 md:top-20 rounded-full bg-white border-2 text-2xl border-gray-400 flex items-center justify-center">
-                    🙊
-                  </div>
-                </div>
-                <div className="font-medium w-20 md:w-28 m-2 break-words">
-                  <div className="flex">
-                    <div style={{lineHeight: "30px", marginTop: "4px"}} className={moderators.includes(swarm.myPeerId) ? "flex-none block bg-yellow-400 text-white font-light text-4xl w-4 h-4 text-center rounded-full" : "hidden"}>*</div>
-                    <div className="flex-none pl-1 overflow-ellipsis w-20 md:w-28">{myInfo.displayName}</div>
-                  </div>
-                </div>
+                <img
+                  className="human-radius border border-gray-300 bg-yellow-50"
+                  src={gravatarUrl(myInfo)}
+                />
+                <div className="text-center mt-2">{myInfo.displayName}</div>
               </li>
             )}
-            {stagePeers.map(peerId => {
-              let {micMuted, inRoom} = peerState[peerId] || {};
+            {audiencePeers.map(peerId => {
+              let {inRoom} = peerState[peerId] || {};
               const peerInfo = identities[peerId] || {id: peerId};
-              // TODO: hadStream is NOT the appropriate condition for showing avatar
-              // need inRoom status from peers
               return (
                 inRoom && (
                   <li
                     key={peerId}
-                    className="relative items-center space-y-1 mt-4"
-                    title={peerId}
+                    className="flex-shrink w-16 h-16 md:w-24 md:h-24 text-xs"
                     style={iModerate ? {cursor: 'pointer'} : undefined}
                     onClick={iModerate ? () => setEditRole(peerId) : undefined}
                   >
-                    <div
-                      className={
-                        speaking.has(peerId)
-                          ? 'human-radius p-1 bg-gray-300'
-                          : 'human-radius p-1 bg-white'
-                      }
-                    >
-                      <div className="human-radius p-1 bg-white">
-                        <img
-                          className="human-radius border border-gray-300 bg-yellow-50 w-20 h-20 md:w-28 md:h-28"
-                          alt={peerId}
-                          src={gravatarUrl(peerInfo)}
-                        />
-                      </div>
-                    </div>
-                    {/* div for showing mute/unmute status */}
-                    <div className={micMuted ? '' : 'hidden'}>
-                      <div className="absolute w-10 h-10 right-0 top-12 md:top-20 rounded-full bg-white border-2 text-2xl border-gray-400 flex items-center justify-center">
-                        🙊
-                      </div>
-                    </div>
-                    <div className="font-medium text-center w-20 md:w-28 m-2 break-words">
-                      <div className="flex">
-                        <div style={{lineHeight: "30px", marginTop: "4px"}} className={moderators.includes(peerId) ? "flex-none block bg-yellow-400 text-white font-light text-4xl w-4 h-4 text-center rounded-full" : "hidden"}>*</div>
-                        <div className="flex-none pl-1 truncate w-20 md:w-28">{peerInfo.displayName}</div>
-                      </div>
-                    </div>
+                    <img
+                      className="human-radius border border-gray-300 bg-yellow-50"
+                      alt={peerId}
+                      src={gravatarUrl(peerInfo)}
+                    />
+                    <div className="text-center mt-2">{peerInfo.displayName}</div>
                   </li>
                 )
               );
             })}
-          </ol>
-        </div>
-
-        <br />
-
-        <h3 className="text-gray-300">Audience</h3>
-        <ol className="flex space-x-4 pt-6">
-          {!iSpeak && (
-            <li
-              className="flex-shrink w-16 h-16 md:w-24 md:h-24 text-xs"
-              style={{cursor: 'pointer'}}
-              onClick={() => setEditIdentity(!editIdentity)}
-            >
+            {/* <li className="flex-shrink w-24 h-24 ring-yellow-500">
               <img
-                className="human-radius border border-gray-300 bg-yellow-50"
-                src={gravatarUrl(myInfo)}
+                className="human-radius border border-gray-300"
+                src="img/avatars/sonic.jpg"
               />
-              <div className="text-center mt-2">{myInfo.displayName}</div>
+            </li> */}
+
+            {/* <li className="flex-shrink w-24 h-24">
+              <img
+                className="human-radius border border-gray-300"
+                src="img/avatars/christoph.jpg"
+              />
             </li>
-          )}
-          {audiencePeers.map(peerId => {
-            let {inRoom} = peerState[peerId] || {};
-            const peerInfo = identities[peerId] || {id: peerId};
-            return (
-              inRoom && (
-                <li
-                  key={peerId}
-                  className="flex-shrink w-16 h-16 md:w-24 md:h-24 text-xs"
-                  style={iModerate ? {cursor: 'pointer'} : undefined}
-                  onClick={iModerate ? () => setEditRole(peerId) : undefined}
-                >
-                  <img
-                    className="human-radius border border-gray-300 bg-yellow-50"
-                    alt={peerId}
-                    src={gravatarUrl(peerInfo)}
-                  />
-                  <div className="text-center mt-2">{peerInfo.displayName}</div>
-                </li>
-              )
-            );
-          })}
-          {/* <li className="flex-shrink w-24 h-24 ring-yellow-500">
-            <img
-              className="human-radius border border-gray-300"
-              src="img/avatars/sonic.jpg"
-            />
-          </li> */}
-
-          {/* <li className="flex-shrink w-24 h-24">
-            <img
-              className="human-radius border border-gray-300"
-              src="img/avatars/christoph.jpg"
-            />
-          </li>
-          <li className="flex-shrink w-24 h-24">
-            <img
-              className="human-radius border border-gray-300"
-              src="img/avatars/tosh.jpg"
-            />
-          </li> */}
-        </ol>
-
-        <div className="navigation">
-          <div className="flex">
-            <button
-              onClick={() => state.set('micMuted', !micMuted)}
-              className="select-none h-12 mr-2 px-6 text-lg text-black bg-yellow-200 rounded-lg focus:shadow-outline active:bg-yellow-300 flex-grow mt-20"
-            >
-              {micOn
-                ? micMuted
-                  ? "🙊 You're silent"
-                  : "🐵 You're on"
-                : "🙊 You're off"}
-            </button>
-          </div>
-
-          <br />
-
-          <button
-            onClick={() => state.set('soundMuted', !soundMuted)}
-            className="select-none h-12 px-6 text-lg text-black bg-gray-200 rounded-lg focus:shadow-outline active:bg-gray-300 flex-grow"
-          >
-            {soundMuted ? '🔇' : '🔊'} {soundMuted ? 'Off' : 'On'}
-          </button>
-
-          <br />
-          <br />
-
-          <div className="flex relative">
-            {showShareInfo && (
-              <span
-                style={{
-                  position: 'absolute',
-                  top: '-20px',
-                  left: '2px',
-                  fontSize: '13px',
-                }}
-              >
-                Link copied to clipboard!
-              </span>
-            )}
-            <button
-              onClick={() => {
-                if (navigator.share) {
-                  navigator.share({
-                    title: name || 'A Jam room',
-                    text: 'Hi, join me in this room on Jam.',
-                    url: location.href,
-                  });
-                } else {
-                  copyToClipboard(location.href);
-                  setShowShareInfo(true);
-                  setTimeout(() => setShowShareInfo(false), 2000);
-                }
-              }}
-              className="select-none h-12 px-6 text-lg text-black bg-gray-200 rounded-lg focus:shadow-outline active:bg-gray-300"
-            >
-              ✉️&nbsp;Share
-            </button>
-
-            <button className="select-none hidden h-12 px-6 text-lg text-black bg-gray-200 rounded-lg focus:shadow-outline active:bg-gray-300 flex-grow">
-              ✋🏽&nbsp;Raise&nbsp;hand
-            </button>
-          </div>
-
-          <br />
-          <br />
-          <br />
-
-          <button
-            className="select-none h-12 px-6 text-lg text-black bg-gray-200 rounded-lg focus:shadow-outline active:bg-gray-300"
-            onClick={() => leaveRoom(roomId)}
-          >
-            🚪 Leave quietly
-          </button>
+            <li className="flex-shrink w-24 h-24">
+              <img
+                className="human-radius border border-gray-300"
+                src="img/avatars/tosh.jpg"
+              />
+            </li> */}
+          </ol>
         </div>
 
         <br />
