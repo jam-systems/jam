7ce9b1f406e6d4e8a60aae21f75f73543ac9aaf5
Christoph Witzany <christoph@web.crofting.com>
2021-02-23 18:40:11 +0100

    Add types for metrics

--- pantry/routes/metrics.js
+++ pantry/routes/metrics.js
@@ -8,10 +8,13 @@ const { roomCount, identityCount } = require('../services/redis');
 const router = express.Router();
 
 router.get('', async function(req, res, next) {
-
+    res.type('text/plain; version=0.0.4');
     res.send(
+        `# TYPE jam_rooms_total counter \n` +
         `jam_rooms_total ${await roomCount()}\n` +
+        `# TYPE jam_identities_total counter \n` +
         `jam_identities_total ${await identityCount()}\n` +
+        `# TYPE jam_users_active gauge \n` +
         `jam_users_active ${await activeUserCount()}\n`
     )
 });
