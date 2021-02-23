28c0821184e05bfea967e8fa87fc26e187489bad
Thomas Schranz <thomas@magic.do>
2021-02-23 22:58:46 +0100

    room: increade height of audience peers

--- ui/views/Room.jsx
+++ ui/views/Room.jsx
@@ -272,7 +272,7 @@ export default function Room({room, roomId}) {
           <ol className="flex flex-wrap space-x-4 pt-6">
             {!iSpeak && (
               <li
-                className="flex-none w-16 h-16 md:w-24 md:h-24 text-xs"
+                className="flex-none w-16 h-32 md:w-24 md:h-36 text-xs"
                 style={{cursor: 'pointer'}}
                 onClick={() => setEditIdentity(!editIdentity)}
               >
@@ -298,7 +298,7 @@ export default function Room({room, roomId}) {
                   <li
                     key={peerId}
                     title={peerInfo.displayName}
-                    className="flex-none w-16 h-16 md:w-24 md:h-24 text-xs"
+                    className="flex-none w-16 h-32 md:w-24 md:h-36 text-xs"
                     style={iModerate ? {cursor: 'pointer'} : undefined}
                     onClick={iModerate ? () => setEditRole(peerId) : undefined}
                   >
