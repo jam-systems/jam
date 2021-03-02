d979b336460b0cd3542ea6e44d70b4fa6ac5cff9
Thomas Schranz <thomas@magic.do>
2021-03-02 23:33:43 +0100

    change default button text color to white

--- ui/views/Room.jsx
+++ ui/views/Room.jsx
@@ -335,7 +335,7 @@ function hexToRGB(hex, alpha) {
 }
 
 function isDark(hex) {
-  if (!hex) return false;
+  if (!hex) return true;
   const r = parseInt(hex.slice(1, 3), 16);
   const g = parseInt(hex.slice(3, 5), 16);
   const b = parseInt(hex.slice(5, 7), 16);
