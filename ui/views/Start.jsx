cce1dfa5623a0293afe551bf4baeb74949e94291
Thomas Schranz <thomas@magic.do>
2021-03-03 12:23:40 +0100

    hide optional form fields on start (editing possible within a room), remove friction

--- ui/views/Start.jsx
+++ ui/views/Start.jsx
@@ -76,65 +76,71 @@ export default function Start({urlRoomId, roomFromURIError}) {
 
         <h1>Start a Room</h1>
 
+        <p className="text-gray-600">
+          Click on the button below to start a room.
+        </p>
+
         <form className="pt-12" onSubmit={submit}>
-          <input
-            className="rounded placeholder-gray-400 bg-gray-50 w-full md:w-96"
-            type="text"
-            placeholder="Room topic"
-            value={name}
-            name="jam-room-topic"
-            autoComplete="off"
-            onChange={e => {
-              setName(e.target.value);
-            }}
-          ></input>
-          <div className="p-2 text-gray-500 italic">
-            Pick a topic to talk about.{' '}
-            <span className="text-gray-400">(optional)</span>
-          </div>
-          <br />
-          <textarea
-            className="rounded placeholder-gray-400 bg-gray-50 w-full md:w-full"
-            placeholder="Room description"
-            value={description}
-            name="jam-room-description"
-            autoComplete="off"
-            rows="2"
-            onChange={e => {
-              setDescription(e.target.value);
-            }}
-          ></textarea>
-          <div className="p-2 text-gray-500 italic">
-            Describe what this room is about.{' '}
-            <span className="text-gray-400">
-              (optional) (supports{' '}
-              <a
-                className="underline"
-                href="https://guides.github.com/pdfs/markdown-cheatsheet-online.pdf"
-                target="_blank"
-              >
-                Markdown
-              </a>
-              )
-            </span>{' '}
-            <span onClick={() => setShowAdvanced(!showAdvanced)}>
-              {/* heroicons/gift */}
-              <svg
-                style={{cursor: 'pointer'}}
-                className="pb-1 h-5 w-5 inline-block"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M12 8v13m0-13V6a2 2 0 112 2h-2zm0 0V5.5A2.5 2.5 0 109.5 8H12zm-7 4h14M5 12a2 2 0 110-4h14a2 2 0 110 4M5 12v7a2 2 0 002 2h10a2 2 0 002-2v-7"
-                />
-              </svg>
-            </span>
+          <div className="hidden">
+            <input
+              className="rounded placeholder-gray-400 bg-gray-50 w-full md:w-96"
+              type="text"
+              placeholder="Room topic"
+              value={name}
+              name="jam-room-topic"
+              autoComplete="off"
+              onChange={e => {
+                setName(e.target.value);
+              }}
+            ></input>
+            <div className="p-2 text-gray-500 italic">
+              Pick a topic to talk about.{' '}
+              <span className="text-gray-400">(optional)</span>
+            </div>
+            <br />
+            <textarea
+              className="rounded placeholder-gray-400 bg-gray-50 w-full md:w-full"
+              placeholder="Room description"
+              value={description}
+              name="jam-room-description"
+              autoComplete="off"
+              rows="2"
+              onChange={e => {
+                setDescription(e.target.value);
+              }}
+            ></textarea>
+            <div className="p-2 text-gray-500 italic">
+              Describe what this room is about.{' '}
+              <span className="text-gray-400">
+                (optional) (supports{' '}
+                <a
+                  className="underline"
+                  href="https://guides.github.com/pdfs/markdown-cheatsheet-online.pdf"
+                  target="_blank"
+                >
+                  Markdown
+                </a>
+                )
+              </span>{' '}
+              <span onClick={() => setShowAdvanced(!showAdvanced)}>
+                {/* heroicons/gift */}
+                <svg
+                  style={{cursor: 'pointer'}}
+                  className="pb-1 h-5 w-5 inline-block"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M12 8v13m0-13V6a2 2 0 112 2h-2zm0 0V5.5A2.5 2.5 0 109.5 8H12zm-7 4h14M5 12a2 2 0 110-4h14a2 2 0 110 4M5 12v7a2 2 0 002 2h10a2 2 0 002-2v-7"
+                  />
+                </svg>
+              </span>
+            </div>
           </div>
 
           {/* advanced Room options */}
@@ -209,7 +215,7 @@ export default function Start({urlRoomId, roomFromURIError}) {
 
           <button
             onClick={submit}
-            className="select-none mt-6 h-12 px-6 text-lg text-black bg-gray-200 rounded-lg focus:shadow-outline active:bg-gray-300"
+            className="select-none h-12 px-6 text-lg text-black bg-gray-200 rounded-lg focus:shadow-outline active:bg-gray-300"
           >
             🌱 Start room
           </button>
