c04c00803b9c5ad47a1f6ee8f362f17b8e819c8a
Gregor <gregor.mitscha-baude@gmx.at>
2021-03-12 13:31:54 +0100

    much, much, much better console logs by adding spaces

--- ui/logic/util.js
+++ ui/logic/util.js
@@ -1,6 +1,23 @@
-export {arrayRemove};
+import {on} from 'minimal-state';
+
+export {arrayRemove, debug};
 
 function arrayRemove(arr, el) {
   let i = arr.indexOf(el);
   if (i !== -1) arr.splice(i, 1);
+}
+
+function debug(state) {
+  let timeout = null;
+  on(state, (key, value, oldValue) => {
+    clearTimeout(timeout);
+    setTimeout(() => {
+      console.log('\n----------------\n\n');
+    }, 10);
+    if (oldValue !== undefined) {
+      console.log(key, oldValue, '->', value);
+    } else {
+      console.log(key, value);
+    }
+  });
 }
