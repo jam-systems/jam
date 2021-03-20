7e0b24e49ac655b0ac613cef3f824a66770b85f2
Thomas Schranz <thomas@magic.do>
2021-03-20 22:44:57 +0100

    navigation: show share symbol depending on platform

--- ui/views/Navigation.jsx
+++ ui/views/Navigation.jsx
@@ -7,8 +7,11 @@ import identity from '../logic/identity';
 import {sendReaction, raiseHand} from '../logic/reactions';
 import EditRole, {EditSelf} from './EditRole';
 import {breakpoints, useWidth} from '../logic/tailwind-mqp';
+import UAParser from 'ua-parser-js';
 
 const reactionEmojis = ['❤️', '💯', '😂', '😅', '😳', '🤔'];
+
+var userAgent = UAParser();
 
 let navigationStyle = {
   position: 'fixed',
@@ -195,12 +198,23 @@ export default function Navigation({
         >
           {/* heroicons/share-small */}
           <svg
-            className="text-gray-600 w-5 h-5"
+            className={userAgent.os.name == "Android" ? "text-gray-600 w-5 h-5" : "hidden"}
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 20 20"
             fill="currentColor"
           >
             <path d="M15 8a3 3 0 10-2.977-2.63l-4.94 2.47a3 3 0 100 4.319l4.94 2.47a3 3 0 10.895-1.789l-4.94-2.47a3.027 3.027 0 000-.74l4.94-2.47C13.456 7.68 14.19 8 15 8z" />
+          </svg>
+          <svg
+            className={userAgent.os.name == "Android" ? "hidden" : "text-gray-600 w-5 h-5"}
+            viewBox="0 0 512 512"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+          >
+            <g id="Solid">
+              <path d="m182.461 155.48 49.539-49.539v262.059a24 24 0 0 0 48 0v-262.059l49.539 49.539a24 24 0 1 0 33.941-33.941l-90.509-90.51a24 24 0 0 0 -33.942 0l-90.509 90.51a24 24 0 1 0 33.941 33.941z"/>
+              <path d="m464 232a24 24 0 0 0 -24 24v184h-368v-184a24 24 0 0 0 -48 0v192a40 40 0 0 0 40 40h384a40 40 0 0 0 40-40v-192a24 24 0 0 0 -24-24z"/>
+            </g>
           </svg>
         </button>
 
