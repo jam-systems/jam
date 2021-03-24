cca7592d67bc175c51b92ccbbb38fb70135147f0
Christoph Witzany <christoph@web.crofting.com>
2021-03-24 08:45:43 +0100

    Implement server admins and restriction of room creation

--- pantry/app.js
+++ pantry/app.js
@@ -2,81 +2,17 @@ const express = require('express');
 const cors = require('cors');
 const logger = require('morgan');
 
-const verifyIdentities = require("./verifications");
 const indexRouter = require('./routes/index');
 const metricsRouter = require('./routes/metrics');
 
-const {verify, isModerator} = require('./auth');
-const {controller, permitAllAuthenticator} = require('./routes/controller');
+const {roomAuthenticator, identityAuthenticator} = require('./auth');
+const {controller} = require('./routes/controller');
 const modMessageRouter = require('./routes/modMessage');
 const app = express();
 
 app.use(logger('dev'));
 app.use(cors());
 app.use(express.json({limit: "500kb"}));
-
-
-const roomAuthenticator = {
-    ...permitAllAuthenticator,
-    canPost: (req, res, next) => {
-        const roomId = req.params.id;
-        if(/^[\w-]{4,}$/.test(roomId)) {
-            next();
-        } else {
-            res.sendStatus(403);
-        }
-    },
-    canPut: async (req, res, next) => {
-        const roomId = req.params.id;
-
-        if(!req.header("Authorization")) {
-            res.sendStatus(401);
-            return
-        }
-        if(!isModerator(req, roomId)) {
-            res.sendStatus(403);
-            return
-        }
-        next();
-    }
-}
-
-const identityAuthenticator = {
-    ...permitAllAuthenticator,
-    canPut: async (req, res, next) => {
-        const authHeader = req.header("Authorization");
-
-        if(!authHeader) {
-            res.sendStatus(401);
-            return
-        }
-
-        const token = authHeader.substring(6);
-        if(!verify(token, req.params.id)) {
-            res.sendStatus(403);
-            return
-        }
-
-        if(req.body.identities) {
-            try {
-                await verifyIdentities(req.body.identities, req.params.id);
-                next();
-            } catch(error) {
-                res.status(400).json(
-                    {
-                        success: false,
-                        error: {
-                            code: "identity-verification-failed",
-                            message: error.message
-                        }
-                    });
-            }
-        } else {
-            next();
-        }
-    },
-}
-
 
 app.use('/', indexRouter);
 app.use('/metrics', metricsRouter);
--- pantry/config.js
+++ pantry/config.js
@@ -1,10 +1,12 @@
 require('dotenv').config()
 
-const jamHost = process.env.JAM_HOST
+const jamHost = process.env.JAM_HOST || 'beta.jam.systems'
 const local = process.env.LOCAL
+const restrictRoomCreation = !!process.env.JAM_RESTRICT_ROOM_CREATION
 
 
 module.exports = {
     jamHost,
-    local
+    local,
+    restrictRoomCreation
 }
