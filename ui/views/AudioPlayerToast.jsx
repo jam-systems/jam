c88342baf234d9270963b59e7b25e96e48fd8e97
Gregor <gregor.mitscha-baude@gmx.at>
2021-03-27 21:23:19 +0100

    minor react fix

--- ui/views/AudioPlayerToast.jsx
+++ ui/views/AudioPlayerToast.jsx
@@ -22,7 +22,7 @@ export default function AudioPlayerToast({close, audio, name}) {
   };
   return (
     <div
-      class="mt-40 w-80"
+      className="mt-40 w-80"
       style={{
         position: 'absolute',
         zIndex: '10',
@@ -31,7 +31,7 @@ export default function AudioPlayerToast({close, audio, name}) {
       }}
     >
       <div
-        class="bg-gray-500 rounded-lg p-6"
+        className="bg-gray-500 rounded-lg p-6"
         ref={el => setElement(el)}
         style={{
           position: 'relative',
@@ -47,28 +47,30 @@ export default function AudioPlayerToast({close, audio, name}) {
           }}
         >
           <div>
-            <div class="text-white font-semibold pb-6">
+            <div className="text-white font-semibold pb-6">
               {/*  heroicons/music-note */}
               <svg
                 className="w-5 h-5 inline mr-2 -mt-1"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
-                stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 19V6l12-3v13M9 19c0 1.105-1.343 2-3 2s-3-.895-3-2 1.343-2 3-2 3 .895 3 2zm12-3c0 1.105-1.343 2-3 2s-3-.895-3-2 1.343-2 3-2 3 .895 3 2zM9 10l12-3" />
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M9 19V6l12-3v13M9 19c0 1.105-1.343 2-3 2s-3-.895-3-2 1.343-2 3-2 3 .895 3 2zm12-3c0 1.105-1.343 2-3 2s-3-.895-3-2 1.343-2 3-2 3 .895 3 2zM9 10l12-3"
+                />
               </svg>
               You are streaming to the room
             </div>
-
-
           </div>
           <div onClick={end} style={{cursor: 'pointer'}}>
             <CloseSvg color="white" />
           </div>
         </div>
-        <div class="mb-3 text-gray-200 text-center">
-          {name || ''}
-        </div>
+        <div className="mb-3 text-gray-200 text-center">{name || ''}</div>
       </div>
     </div>
   );
