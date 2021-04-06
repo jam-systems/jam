ceccdd69bb332e1305bedf439c9d96d337997428
Thomas Schranz <thomas@magic.do>
2021-04-06 13:14:06 +0200

    use text for calendar info to avoid clash with call to action button

--- ui/views/EnterRoom.jsx
+++ ui/views/EnterRoom.jsx
@@ -88,7 +88,7 @@ export default function EnterRoom({
         </button>
 
         <a
-          className={schedule ? "block mt-5 text-center h-12 p-3 px-6 text-lg bg-gray-200 rounded-lg focus:shadow-outline active:bg-gray-200" : "hidden"}
+          className={schedule ? "block mt-5 text-center h-12 p-3 px-6 text-lg text-gray-500" : "hidden"}
           href={`/${roomId}.ics`}
           download={`${(name || 'room')}.ics`}
         >
