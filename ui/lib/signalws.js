1ac51d56df39eb4283d4b2af9b8123148aab64b6
Gregor <gregor.mitscha-baude@gmx.at>
2021-04-27 14:09:27 +0200

    iterate ws client

--- ui/lib/signalws.js
+++ ui/lib/signalws.js
@@ -1,4 +1,5 @@
-import {emit, on} from 'use-minimal-state';
+import {emit, is, on} from 'use-minimal-state';
+import {until} from '../logic/util';
 
 export default function signalws({url, roomId, myPeerId, subscriptions = []}) {
   if (!url) throw new Error('signaling url required');
@@ -12,22 +13,29 @@ export default function signalws({url, roomId, myPeerId, subscriptions = []}) {
 
   let ws = new WebSocket(url);
 
+  ws.addEventListener('open', () => {
+    is(hub, 'opened', true);
+  });
   ws.addEventListener('message', ({data}) => {
-    console.log('ws message', data);
     let msg = decode(data);
     if (msg === undefined) return;
     let {t: topic, d} = msg;
+    if (d?.peerId !== myPeerId) {
+      console.log('ws message', data);
+    }
     emit(hub, topic, d);
   });
 
   const hub = {
+    opened: false,
+    myPeerId,
     ws,
     subs: subscriptions,
     subscribe(topic, onMessage) {
-      subscribe(hub, topic, onMessage);
+      return subscribe(hub, topic, onMessage);
     },
     broadcast(topic, message = {}) {
-      broadcast(hub, topic, message);
+      return broadcast(hub, topic, message);
     },
     broadcastAnonymous: (...args) => hub.broadcast(...args),
     subscribeAnonymous: (...args) => hub.subscribe(...args),
@@ -42,23 +50,26 @@ function subscribe(hub, topic, onMessage) {
   let {ws, subs} = hub;
   if (!subs.includes(topic)) {
     subs.push(topic);
-    let send = () => ws.send(JSON.stringify({s: topic}));
-    if (ws.readyState === WebSocket.OPEN) {
-      send();
-    } else {
-      ws.addEventListener('open', send);
-    }
+    until(hub, 'opened').then(() => send(ws, {s: topic}));
   }
   return on(hub, topic, data => onMessage(data));
   // TODO: send unsubscribe msg on unsubscribing
 }
 
-function broadcast({ws}, topic, message) {
-  ws.send(JSON.stringify({t: topic, d: message}));
+async function broadcast(hub, topic, message) {
+  let {ws, myPeerId} = hub;
+  await until(hub, 'opened');
+  send(ws, {t: topic, d: {...message, peerId: myPeerId}});
 }
 
 function close({ws}) {
   ws.close(1000);
+}
+
+function send(ws, msg) {
+  msg = JSON.stringify(msg);
+  console.log('ws sending', msg);
+  ws.send(msg);
 }
 
 function decode(data) {
