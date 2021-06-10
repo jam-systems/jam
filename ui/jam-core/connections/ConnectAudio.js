4e322632d37471845d080180e6a2224e00425668
gregor <gregor.mitscha-baude@gmx.at>
2021-06-10 22:06:46 +0200

    small bit of mediasoup progress

--- ui/jam-core/connections/ConnectAudio.js
+++ ui/jam-core/connections/ConnectAudio.js
@@ -1,13 +1,21 @@
 import {} from 'mediasoup-client';
 import {addLocalStream} from '../../lib/swarm';
-import {use, useRootState} from '../../lib/state-tree';
+import {use, useRootState, useEvent} from '../../lib/state-tree';
 
 export default function ConnectAudio({swarm}) {
   let sendingStream = null;
+  const {serverEvent} = swarm;
 
   return function ConnectAudio({roomId, shouldSend, shouldReceive}) {
     let localStream = useRootState('myAudio');
+
     let remoteStreams = use(swarm, 'remoteStreams');
+    let [isConsumer, consumer, accept] = useEvent(serverEvent, 'new-consumer');
+    if (isConsumer) {
+      console.log('got new consumer', consumer);
+      // do something in mediasoup with the consumer
+      // accept()
+    }
 
     shouldSend = localStream && shouldSend;
 
