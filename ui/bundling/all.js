20828b1c9cc0b0b4277d16977bd5f48f66de351a
Gregor <gregor.mitscha-baude@gmx.at>
2021-10-01 21:14:18 +0200

    bump watsign for smaller npm install

--- ui/bundling/all.js
+++ ui/bundling/all.js
@@ -11,5 +11,7 @@ import 'tweetnacl';
 import 'tweetnacl-util';
 import 'compact-base64';
 import 'simple-signed-records-engine';
+import 'watsign';
+import 'fast-base64';
 
 import 'mediasoup-client';
