440f5a6c27956d223f113addc63e851c4f2cddae
Christoph Witzany <christoph@web.crofting.com>
2022-08-12 11:31:40 +0200

    Add jam-video example

--- ui/examples/jam-video/App.jsx
+++ ui/examples/jam-video/App.jsx
@@ -1,7 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import {render} from 'react-dom';
-import {JamProvider, useJam, use} from '../../jam-core-react';
-export {use} from 'use-minimal-state';
+import {JamProvider, useJam, use} from 'jam-core-react';
 
 const jamConfig = {
   domain: 'beta.jam.systems',
@@ -16,8 +15,21 @@ render(
   document.querySelector('#root')
 );
 
+const Video = ({stream}) => {
+  const videoRef = React.createRef();
+
+  useEffect(() => {
+    if (videoRef.current) videoRef.current.srcObject = stream;
+  }, [stream, videoRef]);
+
+  return <video style={{height: 100, width: 100}} ref={videoRef} autoPlay />;
+};
+
 function App() {
-  const [state, {createRoom, setProps, enterRoom, leaveRoom}] = useJam();
+  const [
+    state,
+    {createRoom, setProps, enterRoom, leaveRoom, addPresenter},
+  ] = useJam();
   let [
     roomId,
     speaking,
@@ -26,6 +38,8 @@ function App() {
     iAmSpeaker,
     peers,
     peerState,
+    myVideo,
+    remoteVideoStreams,
   ] = use(state, [
     'roomId',
     'speaking',
@@ -34,11 +48,26 @@ function App() {
     'iAmSpeaker',
     'peers',
     'peerState',
+    'myVideo',
+    'remoteVideoStreams',
   ]);
+
+  const myVideoRef = React.createRef();
+  useEffect(() => {
+    // Let's update the srcObject only after the ref has been set
+    // and then every time the stream prop updates
+    if (myVideoRef.current) myVideoRef.current.srcObject = myVideo;
+  }, [myVideo, myVideoRef]);
 
   let hash = location.hash.slice(1) || null;
   let [potentialRoomId, setPotentialRoomId] = useState(hash);
   let nJoinedPeers = peers.filter(id => peerState[id]?.inRoom).length;
+
+  function stream(e) {
+    setProps({userInteracted: true});
+    e.preventDefault();
+    addPresenter(roomId, myId);
+  }
 
   function submit(e) {
     setProps({userInteracted: true});
@@ -68,6 +97,10 @@ function App() {
     };
   }, [setProps, state]);
 
+  const videoElements = remoteVideoStreams.map(stream => (
+    <Video stream={stream.stream} />
+  ));
+
   return (
     <>
       <form onSubmit={submit} style={{marginBottom: '4px'}}>
@@ -82,12 +115,26 @@ function App() {
         <button onClick={submit} disabled={!(potentialRoomId?.length > 3)}>
           {inRoom ? 'Leave' : 'Join'}
         </button>
+        <button onClick={stream} disabled={!inRoom}>
+          Start Video
+        </button>
       </form>
       <div>
         <b style={speaking.has(myId) ? {color: 'green'} : undefined}>
           {iAmSpeaker ? 'Speaking' : 'Not speaking'}
         </b>{' '}
         with <b>{nJoinedPeers}</b> other peer{nJoinedPeers === 1 ? '' : 's'}.
+      </div>
+      <div>
+        <video
+          style={{height: 100, width: 100, border: '1px solid black'}}
+          ref={myVideoRef}
+          autoPlay
+        />
+      </div>
+      <div>
+        <h3> Remote Videos </h3>
+        {videoElements}
       </div>
     </>
   );
